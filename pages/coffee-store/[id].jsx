8c5ee98127abc8bbc24937eea0177588ca56ed5f
anohene1 <anoheneisaac@gmail.com>
2022-02-11 23:32:00 +0000

    set up getStaticPaths for dynamic store page

--- pages/coffee-store/[id].jsx
+++ pages/coffee-store/[id].jsx
@@ -1,7 +1,29 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import coffeeStoresData from "../../data/coffee-stores.json";
 
-export default function CoffeeStore() {
+export async function getStaticProps({ params }) {
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find(
+        (store) => store.id.toString() === params.id
+      ),
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { id: "0" } },
+      { params: { id: "1" } },
+      { params: { id: "300" } },
+    ],
+    fallback: false,
+  };
+}
+
+export default function CoffeeStore(props) {
   const router = useRouter();
   return (
     <div>
